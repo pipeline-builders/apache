f0a4d254baf16f143c39521e15df6c9d317a0dfe
Elon Bar-Evan <elon@Elons-MacBook-Pro.local>
2019-01-14 20:08:12 -0700

    update .kitchen.yml and default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+package "apache2"
+
+file '/var/www/html/index.html' do
+  content "HALLELUJAH"
+end
+
+service 'apache2' do
+  action [ :enable, :start ]
+end
